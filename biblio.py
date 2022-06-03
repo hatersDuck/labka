e8b4bb4f4aa82b4b91f9db820c2ed7a400684fc1
hatersDuck <fishlthe@gmail.com>
2022-06-03 22:47:30 +0700

    book complete NO TEST

--- biblio.py
+++ biblio.py
@@ -6,6 +6,7 @@
     [3] - Статья
 """
 import re
+from tabnanny import check
 def biblio(sourse: str) -> str: 
     check = [
         1,1,1,1
@@ -46,18 +47,9 @@ def biblio(sourse: str) -> str:
 
 import pymorphy2
 
-def book (sourse: str):
-    morph = pymorphy2.MorphAnalyzer(lang='ru')
-    text = sourse.split()
-    if (sourse[sourse.find('.') + 1] != ' '):
-        return f"{text[0]}\tПробел после {text[0]} обязателен"
-    sourse = sourse[sourse.find('.') + 2:-1]
-    
-    
-    templateFIO = ["[А-Я][а-я]+\s[А-Я]\.\s[A-Я]\.", "[А-Я][а-я]+\s[А-Я]\.[A-Я]\.", "[А-Я][а-я]+\s[А-Я]\."] #Фамилия И. О. или Фамилия И.О. или Фамилия И.
+def checkFio(sourse, templateFIO, num, start = 0):
     authors = []
-    start = 0
-    check = 0
+    check = start
     while(True):
         for i in range(len(templateFIO)):
             temp = re.search(templateFIO[i], sourse[start:-1])
@@ -72,30 +64,61 @@ def book (sourse: str):
         else: check = start
 
     if (len(authors) == 0):
-        return f"{text[0]}\tУ книги должен быть автор или Фамилия И.О. записаны не правильно"
+        return f"{num}\tУ книги должен быть автор или Фамилия И.О. записаны не правильно"
     if (sourse[0:start].count(',') != len(authors) - 1):
-        return f"{text[0]}\tМежду фамилиями должны быть запятые"
+        return f"{num}\tМежду фамилиями должны быть запятые"
     if (len(authors) > 3):
         if (sourse.find("и др.") < 0):
-            return f"{text[0]}\t Если авторов более 3-х, то отмечают первых трёх, затем пишется [и др.] или просто 'и др.'"
+            return f"{num}\t Если авторов более 3-х, то отмечают первых трёх, затем пишется [и др.] или просто 'и др.'"
+    return None, start, authors
 
+def book (sourse: str):
+    text = sourse.split()
+    if (sourse[sourse.find('.') + 1] != ' '):
+        return f"{text[0]}\tПробел после {text[0]} обязателен"
+    sourse = sourse[sourse.find('.') + 2:-1]
+    
+    exc, start = checkFio(sourse, ["[А-Я][а-я]+\s[А-Я]\.\s[A-Я]\.", "[А-Я][а-я]+\s[А-Я]\.[A-Я]\.", "[А-Я][а-я]+\s[А-Я]\."], text[0])
+    if (exc is not None):
+        return exc
     template = ',\s\d{4}(?=\.\s[\-–]|\s[(])'
     age = re.search(template, sourse)
-
+    pages = re.search("\.\s[\-–]\s\d{1,}\s",sourse[age.span()[0]:-1])
+    if (pages is None):
+        return f"{text[0]}\tСтраницы указаны не верно"
     if (age is None):
         return f"{text[0]}\tНе указан или не правильно оформлен год выпуска"
-    
     title = sourse[start:age.span()[0]]
     city = title[title.rfind('. -') if (title.rfind('. -')>0) else title.rfind('. –'):len(title)]
-
-    if (city == ""):
+    cityReduce = {
+        'Москва': 'М.',
+        'Ленинград':'Л.', 
+        'Санкт-Петербург':'СПб', 
+        'Нижний Новгород':"Н. Новогород", 
+        'Ростов-на-Дону':"Ростов н/Д"
+    }
+    for i in cityReduce:
+        if (city.find(i) > 0):
+            return f"{text[0]}\t{i} необходимо сократить до {cityReduce[i]}"
+    if (city == "" or city.find(':') < 0):
         return f"{text[0]}\tМесто издания должно быть указано корректно"
     
     title = title.replace(city, '')
-    if (sourse.find('/') > 0):
-        pass
-    else:
-        pass
+    check = True
+    if (title.rfind('/ Под ред.') > 0):
+        exc, start = checkFio(title, ["[А-Я]\.\s[A-Я]\.\s[А-Я][а-я]+", "[А-Я]\.[A-Я]\.\s[А-Я][а-я]+"], text[0], start = title.rfind('/ Под ред.') + len('/ Под ред.'))
+        if (exc is not None):
+            return exc
+        check = False
+    if (title.rfind(':') > 0):
+        publish = ['учебник', "монография", "учеб. пособие"]
+        if not(publish[0] in title or publish[1] in title or publish[2] in title):
+            return f"{text[0]}\tТип книги {publish} не найден"
+        check = False
+    if (check):
+        return f"{text[0]}\tНазвание или издательство записаны неверно"
+    
+
 
 
 # « »  
@@ -103,8 +126,8 @@ def book (sourse: str):
     
 
 testStrings = (
-    "1. Первушкин В. И. Губернские статистические комитеты и провинциальная историческая наука / В. И. Первушкин. – Пенза: ПГПУ, 2007. – 214 с.",
-    "99. Ставицкий В. В. Неолит – ранний энеолит лесостепного Посурья и Прихоперья / В.В. Ставицкий, А.А. Хреков. – Саратов: Изд-во Сарат. ун-та, 2003 (Тип. Изд-ва). – 166 с.",
+    "1. Первушкин В. И. Губернские статистические комитеты и провинциальная историческая наука / Под ред. В. И. Первушкин. – Пенза: ПГПУ, 2007. – 214 с.",
+    "99. Ставицкий В. В. Неолит – ранний энеолит лесостепного Посурья и Прихоперья / Под ред. В.В. Ставицкий, А.А. Хреков. – Саратов: Изд-во Сарат. ун-та, 2003 (Тип. Изд-ва). – 166 с.",
     "155. Кравцова Н. А. Актуальные проблемы современной психосоматики // Человек и современный мир. – 2018. – № 11 (24). – С. 3-10.",
     "122. Оформление списка литературы проектной работы [Электронный ресурс]. – URL: https://workproekt.ru/oformlenie-proekta/oformlenie-spiska-literaturyi/ (дата обращения: 31.05.2022).",
     "22. Апажева С.С., Баразбиев М., Геграев Х.К. Организация досуга молодежи: учебник. – Нальчик: КБГУ им. Х.М. Бербекова, 2017. – 134 с."
